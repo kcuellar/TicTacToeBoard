036e1c727131d183497b3fb92dbd650a57e5411d
Dr Kevin Buffardi <kbuffardi@csuchico.edu>
2017-09-20 17:00:01 +0000

    Added more tests, with bug in function toggleTurn

--- TicTacToeBoardTest.cpp
+++ TicTacToeBoardTest.cpp
@@ -24,25 +24,35 @@ class TicTacToeBoardTest : public ::testing::Test
    }
  */
 
-//TEST(TicTacToeBoardTest, sanityCheck)
-//{
-//	ASSERT_TRUE(false);
-//ASSERT_FALSE(true); // failed
-//
-//}
-
 TEST(TicTacToeBoardTest, toggleTurn)
 {
 	TicTacToeBoard tb;
 	ASSERT_NE(tb.toggleTurn(), tb.toggleTurn());
 }
 
-TEST(TicTacToeBoardTest, placePiece)
+TEST(TicTacToeBoardTest, toggleTurn2)
+{
+	TicTacToeBoard tb;
+	Piece turn1 = tb.toggleTurn();
+	tb.toggleTurn();
+	ASSERT_EQ(turn1, tb.toggleTurn());
+}
+
+TEST(TicTacToeBoardTest, placePiece1)
 {
 	TicTacToeBoard tb;
 	Piece turn1 = tb.placePiece(0, 1);
 	Piece turn2 = tb.placePiece(0, 2);
 	ASSERT_NE(turn1, turn2);
+}
+
+TEST(TicTacToeBoardTest, placePiece2)
+{
+	TicTacToeBoard tb;
+	Piece turn1 = tb.placePiece(0, 1);
+	tb.placePiece(0, 2);
+	Piece turn3 = tb.placePiece(2, 2);
+	ASSERT_EQ(turn1, turn3);
 }
 
 TEST(TicTacToeBoardTest, getPiece)
@@ -53,6 +63,26 @@ TEST(TicTacToeBoardTest, getPiece)
 	Piece get1 = tb.getPiece(0,1);
 	Piece get2 = tb.getPiece(0,2);
 	if((turn1 == get1) && (turn2 == get2)) {
+		ASSERT_TRUE(true);
+	} else {
+		ASSERT_TRUE(false);
+	}
+}
+
+TEST(TicTacToeBoardTest, getPieceBlank)
+{
+	TicTacToeBoard tb;
+	if(tb.getPiece(0,0) == ' ') {
+		ASSERT_TRUE(true);
+	} else {
+		ASSERT_TRUE(false);
+	}
+}
+
+TEST(TicTacToeBoardTest, getPieceInvalid)
+{
+	TicTacToeBoard tb;
+	if(tb.getPiece(3,3) == '?') {
 		ASSERT_TRUE(true);
 	} else {
 		ASSERT_TRUE(false);
@@ -83,6 +113,39 @@ TEST(TicTacToeBoardTest, getWinnerr2O)
 	tb.placePiece(2,2);
 	tb.placePiece(1,2);
 	if(tb.getWinner() == 'O') {
+		ASSERT_TRUE(true);
+	}
+}
+
+TEST(TicTacToeBoardTest, getWinnerTie)
+{
+	TicTacToeBoard tb;
+	tb.placePiece(1,1);
+	tb.placePiece(0,2);
+	tb.placePiece(2,0);
+	tb.placePiece(0,0);
+	tb.placePiece(0,1);
+	tb.placePiece(2,1);
+	tb.placePiece(1,0);
+	tb.placePiece(1,2);
+	tb.placePiece(2,2);
+	if(tb.getWinner() == ' ') {
+		ASSERT_TRUE(true);
+	}
+}
+
+TEST(TicTacToeBoardTest, getWinnerInvalid)
+{
+	TicTacToeBoard tb;
+	tb.placePiece(1,1);
+	tb.placePiece(0,2);
+	tb.placePiece(2,0);
+	tb.placePiece(0,0);
+	tb.placePiece(0,1);
+	tb.placePiece(2,1);
+	tb.placePiece(1,0);
+	tb.placePiece(1,2);
+	if(tb.getWinner() == '?') {
 		ASSERT_TRUE(true);
 	}
 }
